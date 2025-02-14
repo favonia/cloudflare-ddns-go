037bb6102afde57e2c07cdb6b0dafde815af38ee
favonia <favonia@gmail.com>
2025-02-14 13:13:25 -0600

    test: improve coverage

--- internal/ipnet/host_test.go
+++ internal/ipnet/host_test.go
@@ -2,6 +2,7 @@
 package ipnet_test
 
 import (
+	"net/netip"
 	"testing"
 
 	"github.com/stretchr/testify/require"
@@ -27,6 +28,36 @@ func TestHostIDDescribe(t *testing.T) {
 		t.Run(name, func(t *testing.T) {
 			t.Parallel()
 			require.Equal(t, tc.expected, tc.input.Describe())
+		})
+	}
+}
+
+func TestHostIDWithPrefix(t *testing.T) {
+	t.Parallel()
+	for name, tc := range map[string]struct {
+		input  ipnet.HostID
+		prefix netip.Prefix
+		ok     bool
+		addr   netip.Addr
+	}{
+		"ip6suffix": {
+			ipnet.IP6Suffix{0x00, 0x00, 0x00, 0x00, 0x44, 0x55, 0x66, 0x77, 0x88, 0x99, 0xaa, 0xbb, 0xcc, 0xdd, 0xee, 0xff},
+			netip.MustParsePrefix("1122::/40"),
+			true,
+			netip.MustParseAddr("1122::55:6677:8899:aabb:ccdd:eeff"),
+		},
+		"mac": {
+			ipnet.EUI48{0xaa, 0xbb, 0xcc, 0xdd, 0xee, 0xff},
+			netip.MustParsePrefix("1122::/24"),
+			true,
+			netip.MustParseAddr("1122::a8bb:ccff:fedd:eeff"),
+		},
+	} {
+		t.Run(name, func(t *testing.T) {
+			t.Parallel()
+			addr, ok := tc.input.WithPrefix(tc.prefix)
+			require.Equal(t, tc.ok, ok)
+			require.Equal(t, tc.addr, addr)
 		})
 	}
 }
